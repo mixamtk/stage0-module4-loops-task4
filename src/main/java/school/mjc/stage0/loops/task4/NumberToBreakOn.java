01d11bfae20682a44d980ef4c503b09c80db30a2
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-07-06 17:29:15 +0300

    Update NumberToBreakOn.java

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
         if (numberToGoUntil < toBreakWith)
-            System.out.println("iterating till the end");
+            System.out.print("iterating till the end");
         else {
-            for (int i = 1; i <= numberToGoUntil; i++) {
+            for (int i = 0; i < numberToGoUntil; i++) {
                 if (i > toBreakWith)
                     break;
                 System.out.println(i);
